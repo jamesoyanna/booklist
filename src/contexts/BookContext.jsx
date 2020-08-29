798882b689dbefeb77bda27d4a24148f313fcd9e
eshoiza <engrjayt200@gmail.com>
2020-08-29 13:04:15 +0100

    implimented a local storage

--- src/contexts/BookContext.jsx
+++ src/contexts/BookContext.jsx
@@ -1,24 +1,17 @@
-import React, {createContext, useState} from 'react';
-import uuid from 'uuid/v1';
+import React, {createContext, useReducer, useEffect} from 'react';
+import { bookReducer } from './../components/reducers/bookReducer';
 export const BookContext  = createContext();
 
 const BookContextProvider = (props) => {
-    const [books, setBooks] = useState([
-        {title: "name of wind", author: "Fani Kayode", id:1},
-        { title: "empire of money", author: "John bush", id: 2},
-        { title: "final battle", author: "Rita Domnic", id: 3}
-    ])
-    
-    const addBook = (title, author)=>{
-        setBooks([...books, {title, author, id:uuid}])
-    }
-
-    const removeBook = (id)=>{
-     setBooks(books.filter(book =>book.id !==id))
-    }
-
+    const [books, dispatch] = useReducer(bookReducer, [], ()=>{
+      const localData = localStorage.getItem('books')
+       return  localData ? JSON.parse(localData) : []
+    });
+    useEffect(()=>{
+      localStorage.setItem('books', JSON.stringify(books));
+    }, [books])
     return ( 
-       <BookContext.Provider value = {{books, addBook, removeBook}}>
+       <BookContext.Provider value = {{books, dispatch}}>
          {props.children}
        </BookContext.Provider>
      );
